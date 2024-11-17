031fa575e44703efdd25049178165c4c1494580f
Ansel Rognlie <ansel@adadevelopersacademy.org>
2024-11-17 15:06:23 -0800

    initial commit migrates from replit

---
+++ README.md
@@ -0,0 +1,49 @@
+# JS Fundamentals Activity
+
+An activity to practice expressing code concepts we know from Python in JavaScript, to accompany the Learning Another Language lesson.
+
+## One-Time Setup
+
+1. Fork and clone this repository.
+
+2. In your terminal, navigate to the project directory.
+
+3. Install the project dependencies.
+
+   ```bash
+   $ npm install
+   ```
+
+## Running the Activity
+
+1. In your terminal, navigate to the project directory.
+
+2. Run the activity.
+
+  - In the terminal from the project directory, run the following command:
+
+    ```bash
+    $ node src/index.js
+    ```
+
+    Or
+
+    ```bash
+    $ npm start
+    ```
+
+  - Or from VS Code, install the **Code Runner** extension and run the file by clicking the play button in the top right corner of the editor.
+
+## Syntax Warnings
+
+The project is configured to use ESLint to provide syntax warnings. ESLint can be run from the project directory using the following command:
+
+```bash
+$ npm run lint
+```
+
+ESLint warnings can be shown in VS Code by installing the **ESLint** extension.
+
+## Activity Directions
+
+Examine the commented Python code in `src/index.py`. Write equivalent JavaScript code in `src/index.js` to accomplish the same tasks.
---
+++ src/index.js
@@ -0,0 +1,74 @@
+// ======================
+//      VARIABLES
+// ======================
+// veggie = "carrot"
+// veggie = "onion"
+
+//
+//
+// =========================
+//      DATA STRUCTURES
+// =========================
+
+// animals = ["tiger", "panda", "raccoon"]
+// animals.append("opossum")
+
+// dog = {"name": "Sprinkles", "age": 3}
+// dog["is_friendly"] = True
+
+//
+//
+// =================
+//       LOOPS
+// =================
+
+// num = 5
+// for i in range(num):
+//    print(i)
+
+// for animal in animals:
+//    print(animal)
+
+// for k, v in dog.items():
+//    print(f"{k}: {v}")
+
+//
+//
+// ===================================
+//       FUNCTIONS / CONDITIONALS
+// ===================================
+
+// def fizzbuzz(num):
+//    if num % 3 == 0 and num % 5 == 0:
+//        print("fizzbuzz")
+//    elif num % 3 == 0:
+//        print("fizz")
+//    elif num % 5 == 0:
+//        print("buzz")
+
+// Old way: Function Declaration
+
+//
+// ES6 way: Function Expression
+
+//
+//
+// ======================
+//       CLASSES
+// ======================
+
+// class Animal:
+//    def __init__(self,species,name):
+//        self.species = species
+//        self.name = name
+//
+//    def __str__(self):
+//        return f"{self.name} is a {self.species}."
+
+// class Dog(Animal):
+//    def __init__(self, name, trick):
+//        super().__init__(species="dog", name=name)
+//        self.trick = trick
+//
+//    def __str__(self):
+//        return f"{super().to_str()} that can {self.trick}"
